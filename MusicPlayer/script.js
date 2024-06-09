cb0032219cbdeb916c10e512e05db16aa04e38ec
Fedyaaa111 <136244101+Fedyaaa111@users.noreply.github.com>
2024-06-09 15:25:22 +0400

    Update script.js

--- MusicPlayer/script.js
+++ MusicPlayer/script.js
@@ -1,20 +1,53 @@
 const display = document.querySelectorAll('.boxs .box');
 const footer = document.querySelector('.footer');
-const cont = document.querySelectorAll('.boxs2 .box2')
+const cont = document.querySelectorAll('.boxs2 .box2');
+const time = document.querySelector('.footer .cont .second.seconds1');
+const handle = document.querySelector('.footer .cont .handle .handle-white');
+const width = document.querySelector('.footer .cont .handle');
+const btn = document.querySelectorAll('.buttons .btn');
 
+let song = new Audio('song.mp3','song.mp3', 'song.mp3');
+let totalSeconds = 0;
+let timerInterval;
 
-let song = new Audio('song.mp3');
-
-cont.forEach(buttons => {
-    buttons.addEventListener('click', () => {
-      footer.style.display = 'block';
-      song.play();
-    })
+cont.forEach(button => {
+  button.addEventListener('click', () => {
+    currentTime();
+  });
 });
 
+  
 
-display.forEach(buttons => {
-    buttons.addEventListener('click', () => {
-      footer.style.display = 'block';
-    })
+btn[1].addEventListener('click', () => {
+  song.pause();
+  clearInterval(timerInterval)
+})
+function currentTime() {
+  footer.style.display = 'block';
+  song.play();
+  clearInterval(timerInterval);
+  timerInterval = setInterval(() => {
+    totalSeconds++;
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+    time.innerText = formatTime(minutes) + ':' + formatTime(seconds);
+    const progress = song.currentTime / song.duration;
+    handle.style.width = `${progress * 100}%`;
+  }, 1000);
+}
+
+song.addEventListener('ended', () => {
+  clearInterval(timerInterval);
+  handle.style.width = null;
+  time.innerText = '';
 });
+
+display.forEach(button => {
+  button.addEventListener('click', () => {
+    currentTime();
+  });
+});
+
+function formatTime(time) {
+  return time < 10 ? '0' + time : time;
+};
